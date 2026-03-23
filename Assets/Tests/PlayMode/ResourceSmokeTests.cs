96da12adb67b592bca8a459e49be098776083f28
VIOET <1427368054@qq.com>
2026-03-23 17:06:32 +0800

    batch 48: add playmode ui resource tests

---
+++ Assets/Tests/PlayMode/ResourceSmokeTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.UIElements;
+
+namespace EndlessRunner.Tests
+{
+    public class ResourceSmokeTests
+    {
+        [UnityTest]
+        public IEnumerator UiResourcesAreAvailable()
+        {
+            VisualTreeAsset gameUi = Resources.Load<VisualTreeAsset>("UI/GameUI");
+            VisualTreeAsset mainMenuUi = Resources.Load<VisualTreeAsset>("UI/MainMenuUI");
+            PanelSettings panelSettings = Resources.Load<PanelSettings>("UI/GamePanelSettings");
+            Font pixelFont = Resources.Load<Font>("UI/PressStart2P-Regular");
+
+            Assert.IsNotNull(gameUi, "UI/GameUI should exist in Resources.");
+            Assert.IsNotNull(mainMenuUi, "UI/MainMenuUI should exist in Resources.");
+            Assert.IsNotNull(panelSettings, "UI/GamePanelSettings should exist in Resources.");
+            Assert.IsNotNull(pixelFont, "UI/PressStart2P-Regular should exist in Resources.");
+
+            yield return null;
+        }
+    }
+}

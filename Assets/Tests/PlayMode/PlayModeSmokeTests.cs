e80167adf93e5805bc7b008dfca426627af4a6ed
VIOET <1427368054@qq.com>
2026-03-23 16:56:35 +0800

    batch 50: add playmode smoke tests

---
+++ Assets/Tests/PlayMode/PlayModeSmokeTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class PlayModeSmokeTests
+{
+    [UnityTest]
+    public IEnumerator Smoke_FrameAdvances()
+    {
+        var startFrame = Time.frameCount;
+        yield return null;
+        Assert.Greater(Time.frameCount, startFrame);
+    }
+
+    [UnityTest]
+    public IEnumerator Smoke_CanCreateAndDestroyGameObject()
+    {
+        var go = new GameObject("PlayModeSmoke");
+        Assert.IsNotNull(go);
+        Object.Destroy(go);
+        yield return null;
+        Assert.IsTrue(go == null);
+    }
+}
